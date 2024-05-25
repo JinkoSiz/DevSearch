2c9f1a10d71d59ab8c2bed285c072383d9c23271
JinkoSiz <zpska1977@mail.ru>
2024-05-25 21:31:27 +0300

    Adding images

--- projects/models.py
+++ projects/models.py
@@ -89,6 +89,7 @@ class Tag(models.Model):
 
 class Network(models.Model):
     name = models.CharField(max_length=200)
+    featured_image = models.ImageField(null=True, blank=True, default='default.jpg')
     created = models.DateTimeField(auto_now_add=True)
     id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
 
